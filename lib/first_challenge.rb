0ef47a1ddfa295e814b37e42cfe1fd74bfa773d3
sami suliman <sami.bco@gmail.com>
2019-08-10 01:00:43 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,12 @@
 def first_challenge
+<<<<<<< HEAD
    epic_tragedy = { 
      :montague => { },
      :capulet => { }
+=======
+   epic_tragedy = 
+   {
+     
+>>>>>>> 672827decff18c6744f767aaa1edfcab86340f28
    }
 end
